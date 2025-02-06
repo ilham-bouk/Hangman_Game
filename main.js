39e5da2516f72f8c1405ca9f3d4ca60b6c635f71
ilham-bouk <150444920+ilham-bouk@users.noreply.github.com>
2025-02-06 11:04:55 +0000

    Update main.js

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ lettersArray.forEach(letter => {
 })
 
 const words = {
-  programming: ["php", "javascript", "go", "scala", "fortran", "roby", "mysql", "python", "c"],
-  movies: ["Prestige", "Inception", "Parasite", "Interstellar", "Whiplash", "Memento", "Coco", "Up", "Happinise"],
+  programming: ["php", "javascript", "go", "scala", "fortran", "mysql", "python", "c"],
+  movies: ["Prestige", "Inception", "Parasite", "Interstellar", "Whiplash", "Memento", "Coco", "Up"],
   people: ["Albert Einstein", "Hitchcock", "Alexander", "Cleopatra", "Mahatma Ghandi"],
   countries: ["Syria", "Palestine", "Yemen", "Egypt", "Bahrain", "Qatar", "Morocco"]
 }
